37c184242bf8f1ad8776fb9d4e2aa5fa6fe7c630
oguzcan <oguz-can.oezcetin@codenorm.de>
2023-07-31 10:09:48 +0200

    -

---
+++ src/app/mock-booking.ts
@@ -0,0 +1,31 @@
+import {Booking} from "./booking";
+export const Bookings: Booking[] = [
+  {
+    id: 1,
+    name: "Oguz-Can Özcetin",
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date("2023-12-12")
+  },
+  {
+    id: 2,
+    name: "Abdulrahman Kara",
+    roomNumber: 101,
+    startDate: new Date(),
+    endDate: new Date("2023-12-12")
+  },
+  {
+    id: 3,
+    name: "Sadettin Cetin",
+    roomNumber: 103,
+    startDate: new Date(),
+    endDate: new Date("2023-12-12")
+  },
+  {
+    id: 4,
+    name: "Sabri Tangül",
+    roomNumber: 104,
+    startDate: new Date(),
+    endDate: new Date("2023-12-12")
+  }
+];
