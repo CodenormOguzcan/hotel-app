402fa9406d9dbd667c5dd642d2b2784cb1342497
oguzcan <oguz-can.oezcetin@codenorm.de>
2023-07-31 10:10:27 +0200

    -

---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Booking} from "../booking";
+import {Bookings} from "../mock-booking";
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent implements OnInit {
+
+  constructor() { }
+
+  bookings = Bookings;
+
+
+  ngOnInit(): void {
+  }
+
+}
